8d3ba5aa827c4b6705a72b291c42352b6604fbb3
Jonas Wallmann <jonaswallmann@hotmail.de>
2020-07-31 13:57:06 +0200

    requiered changes for SLADIM-131

--- dist/cpu-observation-endpoint.model.js
+++ dist/cpu-observation-endpoint.model.js
@@ -2,12 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const uuid_1 = require("uuid");
 class CpuObservationEndpoint {
-    constructor(name, cpuUtilQueryEndpoint, criticalCpuUtilThreshold, cpuObservationFrequencyMilis) {
-        this.id = uuid_1.v4();
+    constructor(name, cpuUtilQueryEndpoint, criticalCpuUtilThreshold, minimalCpuUtilThreshold, cpuObservationFrequencyMilis) {
         this.name = name;
         this.cpuUtilQueryEndpoint = cpuUtilQueryEndpoint;
         this.criticalCpuUtilThreshold = criticalCpuUtilThreshold;
+        this.minimalCpuUtilThreshold = minimalCpuUtilThreshold;
         this.cpuObservationFrequencyMilis = cpuObservationFrequencyMilis;
+        this.id = uuid_1.v4();
     }
 }
 exports.default = CpuObservationEndpoint;
--- src/cpu-observation-endpoint.model.ts
+++ src/cpu-observation-endpoint.model.ts
@@ -2,22 +2,15 @@ import { v4 as uuid } from 'uuid';
 
 export default class CpuObservationEndpoint {
     id: string;
-    name: string;
-    cpuUtilQueryEndpoint: string;
-    criticalCpuUtilThreshold: number;
-    cpuObservationFrequencyMilis: number;
 
     constructor(
-        name: string,
-        cpuUtilQueryEndpoint: string,
-        criticalCpuUtilThreshold: number,
-        cpuObservationFrequencyMilis: number,
+        public name: string,
+        public cpuUtilQueryEndpoint: string,
+        public criticalCpuUtilThreshold: number,
+        public minimalCpuUtilThreshold: number,
+        public cpuObservationFrequencyMilis: number,
     ) {
         // generates a unique identifier with the uuid pacakge
         this.id = uuid();
-        this.name = name;
-        this.cpuUtilQueryEndpoint = cpuUtilQueryEndpoint;
-        this.criticalCpuUtilThreshold = criticalCpuUtilThreshold;
-        this.cpuObservationFrequencyMilis = cpuObservationFrequencyMilis;
     }
 }
