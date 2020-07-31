928b31aeab39d423b03593d683ac883c78b0e651
Jonas Wallmann <jonaswallmann@hotmail.de>
2020-07-31 14:01:22 +0200

    tsc

--- src/cpu-observation-endpoint.model.ts
+++ src/cpu-observation-endpoint.model.ts
@@ -1,13 +1,24 @@
 import { v4 as uuid } from 'uuid';
 
+/**
+ * Contains information about services of which the cpu utilization should be monitored
+ * Used in monitor-frontend and monitoring-cpu-usage.
+ */
 export default class CpuObservationEndpoint {
+
+    /** unique identify for a CpuObservationEndpoint */
     id: string;
 
     constructor(
+        /** Display name for this service */
         public name: string,
+        /** Endpoint of service */
         public cpuUtilQueryEndpoint: string,
+        /** a value that the cpu utilization should not exceed */
         public criticalCpuUtilThreshold: number,
+        /** a value that the cpu utilization should always be above */
         public minimalCpuUtilThreshold: number,
+        /** the frequency of which the oberserver should check the conditions */
         public cpuObservationFrequencyMilis: number,
     ) {
         // generates a unique identifier with the uuid pacakge
